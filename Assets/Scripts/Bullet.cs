7619eb61f1c6e72000efe318a1024b8b76fe0c83
Luzzotica <sterling.long@lztek.io>
2018-07-07 22:27:05 -0600

    Fighter texture changes, shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+
+    public float bulletSpeed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // Set his velocity
+        Vector3 velocity3D = transform.up * bulletSpeed;
+        GetComponent<Rigidbody2D>().velocity = velocity3D;
+
+        // Destroy ourselves after a few seconds
+        Destroy(this, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera playerCamera;
+    public float speed;
 
-    public float speed;             //Floating point variable to store the player's movement speed.
+    public GameObject weaponTip;
+    public GameObject bullet;
+
+    private float fireLast = 0.0f;
+    private float fireSpeed = 0.2f;
+
+    private Vector3 cameraOffset;
 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
@@ -13,10 +21,38 @@ public class PlayerController : MonoBehaviour
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
+
+        // Start the camera with an initial offset
+        cameraOffset = playerCamera.transform.position;
+    }
+
+    private void Update()
+    {
+        // Handle camera movement
+        handleCamera();
+
+        // Handle attack based on player input
+        handleAttack();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
+    {
+        // Handle rotation before everything
+        handleRotation();
+
+        // Handle movement based on input
+        handleMovement();
+    }
+
+    #region Player Handlers
+
+    private void handleCamera()
+    {
+        playerCamera.transform.position = transform.position + cameraOffset;
+    }
+
+    private void handleRotation()
     {
         // Get the mouse from the input
         Vector3 mouseScreen = Input.mousePosition;
@@ -26,7 +62,10 @@ public class PlayerController : MonoBehaviour
 
         // Calculate rotation to point
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg - 90);
+    }
 
+    private void handleMovement()
+    {
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -38,6 +77,23 @@ public class PlayerController : MonoBehaviour
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
-        print(rb2d.velocity);
     }
+
+    private void handleAttack()
+    {
+        // Update our last fire cooldown
+        fireLast += Time.deltaTime;
+
+        // Check if we want to and can fire
+        if (Input.GetButton("Fire1") && fireLast > fireSpeed)
+        {
+            // Reset fireLast
+            fireLast = 0f;
+
+            // Create our bullet
+            GameObject newBullet = Instantiate(bullet, weaponTip.transform.position, weaponTip.transform.rotation);
+        }
+    }
+
+    #endregion
 }
